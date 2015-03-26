f9fd6d713570afd96795dbf334334922d0b0dbb4
Timothy Chen <tnachen@gmail.com>
2015-03-26 12:27:11 -0700

    Support status, kill and submit

--- dcos_spark/cli.py
+++ dcos_spark/cli.py
@@ -4,7 +4,9 @@ Usage:
     dcos spark --help
     dcos spark --info
     dcos spark --version
-    dcos spark run <jarUrl> [<args>...]
+    dcos spark run [<args>...]
+    dcos spark status <submissionId>
+    dcos spark kill <submissionId>
 
 Options:
     --help                  Show this screen
@@ -14,19 +16,31 @@ Options:
 import docopt
 from dcos_spark import constants, dcos, spark_submit
 
+master = "10.51.128.131:7077"
+
 def run_spark_job(args):
-    spark_submit.run("master", args['<jarUrl>'], args['<args>'])
-    return 0
+    return spark_submit.submit_job(master, args['<args>'])
+
+def job_status(args):
+    return spark_submit.job_status(master, args['<submissionId>'])
+
+def kill_job(args):
+    return spark_submit.kill_job(master, args['<submissionId>'])
 
 def main():
     args = docopt.docopt(
         __doc__,
-        version='dcos-spark version {}'.format(constants.version))
+        version='dcos-spark version {}'.format(constants.version),
+        options_first=True)
 
     if args['--info']:
         print('Run and manage Spark jobs')
     elif args['run']:
         return run_spark_job(args)
+    elif args['status']:
+        return job_status(args)
+    elif args['kill']:
+        return kill_job(args)
     else:
         print(__doc__)
         return 1
--- dcos_spark/spark_submit.py
+++ dcos_spark/spark_submit.py
@@ -1,5 +1,31 @@
 from dcos_spark import constants
 import pkg_resources
+import subprocess
+import json
+
+def submit_job(master, args):
+    response = run(master, args)
+    if response[0] != None:
+        print "Run job succeeded. Submission id: " + response[0]['submissionId']
+    return response[1]
+
+def job_status(master, submissionId):
+    response = run(master, ["--status", submissionId])
+    if response[0] != None:
+        print "Submission ID: " + response[0]['submissionId']
+        print "Job status: " + response[0]['driverState']
+    return response[1]
+
+def kill_job(master, submissionId):
+    response = run(master, ["--kill", submissionId])
+    if response[0] != None:
+        if bool(response[0]['success']):
+            success = "succeeded."
+        else:
+            success = "failed."
+        print "Kill job " + success
+        print "Message: " + response[0]['message']
+    return response[1]
 
 """
   This method runs spark_submit with the passed in parameters.
@@ -7,12 +33,12 @@ import pkg_resources
       --master mesos://10.127.131.174:8077 --executor-memory 1G --total-executor-cores 100
      --driver-memory 1G http://10.127.131.174:8000/spark-examples_2.10-1.3.0-SNAPSHOT.jar 30
 """
-def run(master, jar, options):
+def run(master, args):
     submit_file = pkg_resources.resource_filename(
         'dcos_spark',
-        'data/' + constants.spark_version + '/bin/spark-submit.sh')
+        'data/' + constants.spark_version + '/bin/spark-submit')
 
-    command = submit_file + " --deploy-mode cluster --master mesos://" + master + " " + options
+    command = [submit_file, "--deploy-mode", "cluster", "--master", "mesos://" + master] + args
 
     process = subprocess.Popen(
             command,
@@ -21,4 +47,10 @@ def run(master, jar, options):
 
     stdout, stderr = process.communicate()
 
-    return process.returncode
+    if process.returncode != 0:
+        print "Spark submit failed:"
+        print stderr
+        return (None, process.returncode)
+    else:
+        response = json.loads(stderr[stderr.index('{')::])
+        return (response, process.returncode)
