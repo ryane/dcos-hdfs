f3e1d0f314a63f8def95a177afed33105207c50c
Timothy Chen <tnachen@gmail.com>
2015-04-07 17:49:05 -0700

    Use dynamic ports from marathon.

--- dcos_spark/discovery.py
+++ dcos_spark/discovery.py
@@ -42,9 +42,7 @@ def get_spark_webui():
         print "Spark cluster task is not running yet."
         sys.exit(1)
 
-    task_args = spark_task["cmd"].split(" ")
-    webui_port = task_args[task_args.index("--webui-port") + 1]
-    return "http://" + tasks[0]["host"] + ":" + webui_port
+    return "http://" + tasks[0]["host"] + ":" + str(tasks[0]["ports"][1])
 
 
 def get_spark_dispatcher():
@@ -55,6 +53,4 @@ def get_spark_dispatcher():
         print "Spark cluster task is not running yet."
         sys.exit(1)
 
-    task_args = spark_task["cmd"].split(" ")
-    dispatcher_port = task_args[task_args.index("--port") + 1]
-    return tasks[0]["host"] + ":" + dispatcher_port
+    return tasks[0]["host"] + ":" + str(tasks[0]["ports"][0])
